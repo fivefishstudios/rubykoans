57ad9ab4e931feab15c7d43e130c2027e98e02ce
Ruel Oquindo <ruel@fivefish.net>
2013-07-09 07:30:36 -0500

    54% complete

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,9 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  return :equilateral if (a==b) && (b==c) && (c==a)
+  return :isosceles if (a==b) || (b==c) || (c==a)
+  :scalene	# no sides equal
 end
 
 # Error class used in part 2.  No need to change this code.
