22ea00fc39e9e67b3a6b905d67e18c69140d14ba
Ruel Oquindo <ruel@fivefish.net>
2013-07-10 13:58:10 -0500

    alternate solution for DiceSet class

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,19 +2,27 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-class DiceSet
 
+class DiceSet
+  attr_reader :values
   def roll(number_of_dice)
     @values = []
     1.upto(number_of_dice) { |i| @values << Random.new.rand(1..6)  }
     return @values
   end
 
-  def values
-    @values
-  end
-
 end
+
+
+=begin comment
+class DiceSet
+  attr_reader :values
+
+  def roll(number_of_dice)
+    @values = number_of_dice.times.inject([]) { |arr, i| arr << rand(1..6) }
+  end
+end
+=end 
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
